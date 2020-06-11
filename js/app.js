50276a83be2e03b8c61c31b5fb1edd3a0bde6525
Boyd <boyd.land@gmail.com>
2020-06-11 11:31:15 -0400

    disable wrapping of world

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     subdomains: 'abcd',
     maxZoom: 19,
+    noWrap: true
   }).addTo(map);
 
   const legendControl = L.control({
